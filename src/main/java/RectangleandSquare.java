36203e469586038e1c8a815d8d6dc6861030d271
Siri1703 <sirilakshmi1703@gmail.com>
2021-08-18 17:40:37 +0530

    Requirement 1: Rectangle Area with Two Integers

--- src/main/java/RectangleandSquare.java
+++ src/main/java/RectangleandSquare.java
@@ -1,2 +1,12 @@
 public class RectangleandSquare {
+    int length;
+    int breadth;
+    public RectangleandSquare(int l, int b) {
+        this.length=l;
+        this.breadth=b;
+    }
+
+    public int rectangleArea() {
+        return length*breadth;
+    }
 }
--- src/test/java/RectangleandSquareTesting.java
+++ src/test/java/RectangleandSquareTesting.java
@@ -1,2 +1,50 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RectangleandSquareTesting {
+
+   @Nested
+    public  class RectangleAreaTesting
+    {
+        @Test
+        public void toCalculateAreaWhenLengthandBreadtharePositive()
+        {
+            RectangleandSquare obj=new RectangleandSquare(4,3);
+            int expectedArea=12;
+            int actualArea=obj.rectangleArea();
+            assertEquals(expectedArea,actualArea);
+
+        }
+
+        @Test
+        public void toCalculateAreaWhenLengthandBreadtharenegative()
+        {
+            RectangleandSquare obj=new RectangleandSquare(-4,-3);
+            int expectedArea=12;
+            int actualArea=obj.rectangleArea();
+            assertEquals(expectedArea,actualArea);
+
+        }
+        @Test
+    public void toCalculateAreaWhenLengthispositiveandBreadthisNegative()
+    {
+        RectangleandSquare obj=new RectangleandSquare(4,-3);
+        int expectedArea=-12;
+        int actualArea=obj.rectangleArea();
+        assertEquals(expectedArea,actualArea);
+
+    }
+
+        @Test
+        public void toCalculateAreaWhenLengthisNegativeandBreadthisPositive()
+        {
+            RectangleandSquare obj=new RectangleandSquare(-4,3);
+            int expectedArea=-12;
+            int actualArea=obj.rectangleArea();
+            assertEquals(expectedArea,actualArea);
+
+        }
+    }
 }
